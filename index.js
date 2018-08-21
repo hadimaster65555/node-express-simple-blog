418660378b0a28551d029739782e1a43d0ac63c1
hadimaster <hadimaster65555@gmail.com>
2018-08-21 16:32:41 +0700

    add template engine, delete pages folder

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
 const express = require('express');
 const path = require('path');
-
+const expressEdge = require('express-edge');
 
 const app = new express();
 
 app.use(express.static('public'));
+app.use(expressEdge);
+app.set('views',`${__dirname}/views`);
 
 app.get('/', (request,response) => {
-    response.sendFile(path.resolve(__dirname, 'pages/index.html'));
+    // response.sendFile(path.resolve(__dirname, 'pages/index.html'));
+    response.render('index');
 });
 
 app.get('/about', (request, response) => {
-    response.sendFile(path.resolve(__dirname, 'pages/about.html'))
-})
+    // response.sendFile(path.resolve(__dirname, 'pages/about.html'))
+    response.render('about');
+});
 
 app.get('/post', (request, response) => {
-    response.sendFile(path.resolve(__dirname, 'pages/post.html'))
-})
+    // response.sendFile(path.resolve(__dirname, 'pages/post.html'))
+    response.render('post');
+});
 
 app.get('/contact', (request, response) => {
-    response.sendFile(path.resolve(__dirname, 'pages/contact.html'))
-})
+    // response.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+    response.render('contact');
+});
 
 app.listen(3000, () => {
     console.log('App Started');
-})
+});
 
