c87b1bb6219c415d700a657bf5e8c9ab8cbb83d5
hadimaster <hadimaster65555@gmail.com>
2018-08-21 15:39:44 +0700

    add more pages

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const express = require('express');
+const path = require('path');
+
 
 const app = new express();
+
+app.use(express.static('public'));
+
+app.get('/', (request,response) => {
+    response.sendFile(path.resolve(__dirname, 'pages/index.html'));
+});
+
+app.get('/about', (request, response) => {
+    response.sendFile(path.resolve(__dirname, 'pages/about.html'))
+})
+
+app.get('/post', (request, response) => {
+    response.sendFile(path.resolve(__dirname, 'pages/post.html'))
+})
+
+app.get('/contact', (request, response) => {
+    response.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+})
 
 app.listen(3000, () => {
     console.log('App Started');
